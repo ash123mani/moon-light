9f5cd396c9db8da6b459dbe6ba8acc50f805c86d
Ashutosh <ashuma2721@gmail.com>
2021-06-26 13:50:19 +0530

    added ssr for carousel

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from './src/shared/layout'
+import 'react-multi-carousel/lib/styles.css'
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
