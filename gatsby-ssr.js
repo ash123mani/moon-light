e84fb77088f3a3dad3e628f99fef85abe3eea25b
Ashutosh <ashuma2721@gmail.com>
2021-06-26 14:04:39 +0530

    removing ssr config

--- gatsby-ssr.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import Layout from './src/shared/layout'
-import 'react-multi-carousel/lib/styles.css'
-
-export const wrapPageElement = ({ element, props }) => {
-  return <Layout {...props}>{element}</Layout>
-}
