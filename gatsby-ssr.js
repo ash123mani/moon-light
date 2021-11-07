4da8ef31ce1e7547306882003723d98c4c5d9402
ashutosh <ashutosh.tripathi@walmart.com>
2021-11-07 19:52:07 +0530

    update gastby-ssr for mobile view

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,5 +1,16 @@
 import React from 'react'
+import { Context as ResponsiveContext } from 'react-responsive'
 import Layout from './src/shared/layout'
+import { breakpoints } from './src/styles/utils'
+import { renderToString } from 'react-dom/server'
+
+export const replaceRenderer = ({ bodyComponent, replaceBodyHTMLString }) => {
+  const bodyHTML = renderToString(
+    <ResponsiveContext.Provider value={breakpoints}>{bodyComponent}</ResponsiveContext.Provider>,
+  )
+
+  replaceBodyHTMLString(bodyHTML)
+}
 
 export const wrapPageElement = ({ element, props }) => {
   return <Layout {...props}>{element}</Layout>
